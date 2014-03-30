e0fe50a967c7d724e073ec4a9c6a2b278bf3770e
Doug Helferich <dhelf17@gmail.com>
2014-03-30 16:53:55 -0400

    added a method to access all games for a specific team -dh

--- lib/Kgame.rb
+++ lib/Kgame.rb
@@ -3,15 +3,23 @@ require 'httparty'
 class Kgame
   attr_accessor :id, :teams
   def initialize()
-    get_data
+    get_recent_data
   end
 
-  def get_data
+  def get_recent_data
     HTTParty.get('http://www.krossover.com/intelligence/feed/games', :basic_auth => {:username => "SINY", :password => "RSD72aH1uBlIdcX"})
   end
-
+  
+  def get_full_data
+	HTTParty.get('http://www.krossover.com/intelligence/feed/games', :basic_auth => {:username => "SINY", :password => "RSD72aH1uBlIdcX"})
+  end
+  
+  def get_data_by_team(id)
+	HTTParty.get('http://www.krossover.com/intelligence/feed/teams/' + id.to_s + '/games', :basic_auth => {:username => "SINY", :password => "RSD72aH1uBlIdcX"})
+  end
+  
   def data
-    JSON.parse(get_data.body)
+    JSON.parse(get_recent_data.body)
   end
 
   def id
@@ -21,8 +29,12 @@ class Kgame
   def teams
     data.first["teams"]
   end
+  
+  def team_games(id)
+	JSON.parse(get_data_by_team(id))
+	end
 end
 
 game = Kgame.new
 
-puts game.id
+puts game.team_games(35615)
